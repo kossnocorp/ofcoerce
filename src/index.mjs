4c4ffd921b6e66f9983aa2932365575a57e073cb
Sasha Koss <koss@nocorp.me>
2024-05-28 05:51:57 +0800

    Add code comments

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ const optionalSymbol = Symbol();
 
 const helpers = {
   Optional(type) {
+    // Reuse the same optional object to avoid trashing the heap
     !optionals.has(type) && optionals.set(type, { [optionalSymbol]: type });
     return optionals.get(type);
   },
@@ -11,27 +12,39 @@ const helpers = {
 export function coercer(schema) {
   if (typeof schema === "function") schema = schema(helpers);
   return function coerce(value, coercer = schema) {
-    if (typeof coercer === "function") return coercer(value || "");
-    else if (Array.isArray(coercer)) {
+    // The coercer is a function (Boolean, Number, String, etc.), so we can it.
+    if (typeof coercer === "function")
+      return coercer(
+        value || "" // Add empty value to prevent "undefined" strings
+      );
+
+    // The coercer is an array, so we use the first element as the coercer.
+    if (Array.isArray(coercer))
       return (value && value?.map((item) => coerce(item, coercer[0]))) || [];
-    } else if (typeof coercer === "object") {
-      value ||= {};
-      const result = {};
-      // Define getter to access FormData
-      const get =
-        value instanceof FormData
-          ? (key) => value.get(key)
-          : (key) => value[key];
-      for (const key in coercer) {
-        const field = coercer[key];
-        if (optionalSymbol in field) {
-          // [TODO] FormData support for this case
-          if (key in value)
-            result[key] = coerce(get(key), field[optionalSymbol]);
-        } else result[key] = coerce(get(key), field);
+
+    // The coercer is an object and each key must be coerced individually.
+
+    const result = {};
+
+    // Define getter to access FormData
+    value ||= {};
+    const get =
+      value instanceof FormData ? (key) => value.get(key) : (key) => value[key];
+
+    for (const key in coercer) {
+      const field = coercer[key];
+
+      // Handle optional field
+      if (optionalSymbol in field) {
+        // Skip coercing optional field, if it is not present in the value
+        // [TODO] FormData support for this case
+        if (key in value) result[key] = coerce(get(key), field[optionalSymbol]);
       }
-      return result;
+      // Handle required field
+      else result[key] = coerce(get(key), field);
     }
+
+    return result;
   };
 }
 
