228e5ea9551d63fe7eb0cc0df639a592cf14d896
Sasha Koss <koss@nocorp.me>
2025-02-21 09:40:54 +0800

    Set up dev env with Nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Of Coerce! dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # Tools
+              direnv
+              just
+              # Node.js
+              nodejs
+              corepack
+            ];
+          };
+      }
+    );
+}
